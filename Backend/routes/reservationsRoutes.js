0ad20ef95c20da9d582525891e0055d756811043
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2025-09-17 19:54:52 +0300

    patikrinti su post

--- Backend/routes/reservationsRoutes.js
+++ Backend/routes/reservationsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import * as reservationController from "../controllers/reservationController";
+import * as reservationController from "../controllers/reservationController.js";
 import { adminMiddleware } from "../middleware/adminMiddleware.js";
 const router = express.Router();
 
